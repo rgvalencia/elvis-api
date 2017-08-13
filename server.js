2e760ea13c65e96e1b55ee2d5facbb2057e5e730
Rafael Valencia <rgonzalez@applaudostudios.com>
2017-08-13 00:32:54 -0600

    Add body-parser to parse incoming request bodies.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express     = require('express');
 const app         = express();
 const morgan      = require('morgan');
+const bodyParser    = require('body-parser');
 const mongoose    = require('mongoose');
 
 const config = require('./config');
@@ -15,6 +16,10 @@ mongoose.connect(config.db); // connect to database
 mongoose.connection.on('error', error => {
   console.log('MongoDB connection has failed.');
 })
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // use morgan to log requests to the console
 app.use(morgan('dev'));
