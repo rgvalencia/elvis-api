1c155118103a1117209dda0c2560cf0909b60f6f
Rafael Valencia <rgonzalez@applaudostudios.com>
2017-08-13 18:54:57 -0600

    Create users controller to handle sign up. Use express-jwt to validate a JsonWebToken (JWT) and set the req.user with the attributes.

---
+++ app/controllers/users.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const util   = require('../helpers/util');
+const User   = require('../models/user');
+const config = require('../../config/config');
+const jwt    = require('express-jwt'); 
+const utilx = require('util');
+
+module.exports = router => {
+  router
+    .use(jwt({ secret: config.secret})
+    .unless({ path: ['/api/users'] }));
+
+  router.route('/users/:id')
+    .get(function (req, res) {
+      User.findById(req.params.id, function(err, user) {
+        if (err) {
+          res.status(err.statusCode || 500).json(err);
+        } else {
+          res.json(user);
+        }
+      });
+    })
+    .put(function (req, res) {
+      User.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, user) {
+        if (err) {
+          res.status(err.statusCode || 500).json(err);
+        } else {
+          res.json(user);
+        }
+      });
+    });
+
+  router.route('/users')
+    .post(function(req, res) {
+      req.body.password = util.encrypt(req.body.password)
+      
+      User.create(req.body, function (err, user) {
+        if (err) {
+          res.status(err.statusCode || 500).json(err);
+        } else {
+          user = user.toObject(); // swap for a plain javascript object instance
+
+          delete user["password"];
+        
+          res.json(user);
+        }
+      })
+    });
+
+};
