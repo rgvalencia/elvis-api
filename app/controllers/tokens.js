a856f1bce5226c733a2fd55b7e13652e3c83b687
Rafael Valencia <rgonzalez@applaudostudios.com>
2017-08-13 18:47:16 -0600

    Create tokens controller used to create and sign tokens.

---
+++ app/controllers/tokens.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const config = require('../../config/config'),
+      util   = require('../helpers/util'),
+      User   = require('../models/user'),
+      jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+
+module.exports = router => {
+  router.post('/auth/token', function(req, res) {
+    User.findOne({
+      email: req.body.email
+    })
+    .select('+password')
+    .exec(function(err, user) {
+      if (err) throw err;
+
+      if (!user) {
+        res.status(422).json({ success: false, message: 'Authentication failed. User not found.' });
+      } else if (user) {
+        if (util.decrypt(user.password) != req.body.password) {
+          res.status(422).json({ success: false, message: 'Authentication failed. Wrong password.' });
+        } else {
+          res.json({
+            token: jwt.sign(user, config.secret, {
+              expiresIn: '1h'
+            })
+          });
+        }   
+
+      }
+
+    });
+  });
+
+};
